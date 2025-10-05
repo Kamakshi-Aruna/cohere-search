8db4bd901a160db40708fe6732d6af54b79a3178
arunakamakshi <arunakamakshi09@gmail.com>
2025-10-06 02:27:40 +0530

    in memory storage updates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <div className="flex-shrink-0 text-center py-8 px-4">
             <h1 className="text-4xl font-bold mb-2 bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent">
-                Local AI Search Engine
+             Cohere Search Engine
             </h1>
         </div>
     );
